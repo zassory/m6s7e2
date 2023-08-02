61864ff1bbfe589cbba39f44f81499a0df2fdb3d
Nicolas Caceres Latorre <nicolas.programador@gmail.com>
2023-08-01 22:42:40 -0400

    update ok

--- index.js
+++ index.js
@@ -8,13 +8,50 @@ http.createServer( async(req = request, res=response) => {
     const params = new URLSearchParams(searchParams);
 
     if(pathname === '/autos' && req.method === 'GET'){
-        const leerArchivos = await fs.readFile('./datos/autos.txt');
+        const lecturaArchivos = await fs.readFile('./datos/autos.txt');
+        res.write(lecturaArchivos);
+        res.end();
     }
     if(pathname === '/autos' && req.method === 'POST'){
-        
+        const archivoOriginal = await fs.readFile('./datos/autos.txt');
+        const datosOriginales = JSON.parse(archivoOriginal);
+        const id = uuidv4();
+
+        let datosAutos;
+
+        req.on('data', (data) => {
+            datosAutos = JSON.parse(data);
+            console.log(datosAutos);
+        });
+        req.on('end', async() => {
+            datosOriginales[id] = datosAutos;
+            await fs.writeFile('./datos/autos.txt', JSON.stringify(datosOriginales,null,2));
+            res.write("Auto agregado satisfactoriamente");
+            res.end();
+        })
+
     }
     if(pathname === '/autos' && req.method === 'PUT'){
-        
+        const id = params.get('id');
+
+        const datosArchivo = await fs.readFile('./datos/autos.txt');
+        const objetoArchivoOriginal = JSON.parse(datosArchivo);
+
+        let datosParaModificar;
+
+        //Por cada dato en el callback (param)
+        req.on('data',(datos) => {
+            datosParaModificar = JSON.parse(datos);
+        })
+        req.on('end', async() => {
+            const autoOriginal = objetoArchivoOriginal[id];
+            const autoActualizado = {...autoOriginal, ...datosParaModificar};//Estudiar esto
+
+            objetoArchivoOriginal[id] = autoActualizado;
+
+            await fs.writeFile('./datos/autos.txt', JSON.stringify(objetoArchivoOriginal,null,2));
+            res.write(JSON.stringify(autoActualizado,null,2));
+        })
     }
     if(pathname === '/autos' && req.method === 'DELETE'){
         
@@ -23,58 +60,6 @@ http.createServer( async(req = request, res=response) => {
 .listen(3000, ()=> {
     console.log('Conectado al puerto 3000 correctamente');
 });
-
-// http.createServer(async(req = response,res = response) => {
-//     const { searchParams , pathname } = new URL(req.url,`http://${req.headers.host}`);
-//     const params = new URLSearchParams(searchParams);
-    
-//     if(pathname === '/comics' && req.method == 'GET'){
-//         const lecturaArchivos = await fs.readFile('./datos/comics.txt');
-//         res.write(lecturaArchivos);
-//         res.end();
-//     }
-//     if(pathname === '/comics' && req.method == 'POST'){
-//         const archivoOriginal = await fs.readFile('./datos/comics.txt');
-//         const datosOriginales = JSON.parse(archivoOriginal);
-//         const id = uuidv4();
-//         let datosComic;
-
-//         req.on('data',(data)=> {
-//             datosComic = JSON.parse(data);
-//             console.log(datosComic);
-//         });
-//         req.on('end', async()=> {
-//             datosOriginales[id] = datosComic;
-//             await fs.writeFile('./datos/comics.txt',JSON.stringify(datosOriginales,null,2));
-//             res.write("Comic agregado exitosamente");
-//             res.end();
-//         });
-        
-//     }
-//     if(pathname === '/comics' && req.method == 'PUT'){
-//         const id = params.get('id');
-//         //first read the data
-//         const datosArchivo = await fs.readFile('./datos/comics.txt');
-//         //then stop what i bring
-//         const objetoArchivoOriginal = JSON.parse(datosArchivo);
-
-//         let datosParaModificar;
-
-//         req.on('data', (datos) => {
-//             datosParaModificar = JSON.parse(datos);
-//         })
-//         req.on('end', async()=> {
-//             const comicOriginal = objetoArchivoOriginal[id];
-//             const comicActualizado = {...comicOriginal, ...datosParaModificar}
-
-//             objetoArchivoOriginal[id] = comicActualizado;
-
-//             await fs.writeFile('./datos/comics.txt', JSON.stringify(objetoArchivoOriginal,null,2));
-
-//             res.write(JSON.stringify(comicActualizado,null,2));
-//             res.end();
-//         });
-//     }
 //     if(pathname === '/comics' && req.method == 'DELETE'){
 //         const comicsOriginales = await fs.readFile('./datos/comics.txt');
 //         const objetoComicsOriginal = JSON.parse(comicsOriginales);
---
+++ oldindex.js
@@ -0,0 +1,41 @@
+// http.createServer(async(req = response,res = response) => {
+//     const { searchParams , pathname } = new URL(req.url,`http://${req.headers.host}`);
+//     const params = new URLSearchParams(searchParams);
+    
+//     if(pathname === '/comics' && req.method == 'GET'){
+//         const lecturaArchivos = await fs.readFile('./datos/comics.txt');
+//         res.write(lecturaArchivos);
+//         res.end();
+//     }
+//     if(pathname === '/comics' && req.method == 'POST'){
+//         const archivoOriginal = await fs.readFile('./datos/comics.txt');
+//         const datosOriginales = JSON.parse(archivoOriginal);
+//         const id = uuidv4();
+//         let datosComic;
+
+//         req.on('data',(data)=> {
+//             datosComic = JSON.parse(data);
+//             console.log(datosComic);
+//         });
+//     if(pathname === '/comics' && req.method == 'PUT'){
+//         const id = params.get('id');
+//         //first read the data
+//         const datosArchivo = await fs.readFile('./datos/comics.txt');
+//         //then stop what i bring
+//         const objetoArchivoOriginal = JSON.parse(datosArchivo);
+
+//         let datosParaModificar;
+//         req.on('data', (datos) => {
+//             datosParaModificar = JSON.parse(datos);
+//         })
+//         req.on('end', async()=> {
+//             const comicOriginal = objetoArchivoOriginal[id];
+//             const comicActualizado = {...comicOriginal, ...datosParaModificar}
+//             objetoArchivoOriginal[id] = comicActualizado;
+
+//             await fs.writeFile('./datos/comics.txt', JSON.stringify(objetoArchivoOriginal,null,2));
+
+//             res.write(JSON.stringify(comicActualizado,null,2));
+//             res.end();
+//         });
+//     }
